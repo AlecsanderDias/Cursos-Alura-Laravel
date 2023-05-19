d1ef9b5c540e30e22a51c92b2eab625723e6e09b
Alecsander <alecsanderdias@hotmail.com>
2023-05-19 18:51:49 -0300

    Finalizado Módulo 4 do Curso de Laravel: criando uma aplicação com MVC.

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -1,4 +1,8 @@
 <x-layout title="Séries">
-    <a href="/series/criar">Adicionar</a>
-    {{ $teste }}
+    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
+    <ul class="list-group">
+        @foreach ($teste as $test)
+        <li class="list-group-item">{{ $test }}</li>
+        @endforeach
+    </ul>
 </x-layout>
